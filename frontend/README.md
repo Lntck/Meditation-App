c87bfd669bf35422f52f62f46b8787452514d367
wrzdx <7zhhovalyg@mail.ru>
2025-07-16 14:01:29 +0300

    Project migration and restructuring
    This commit represents the consolidation and migration of our meditation
    application development from a private GitLab repository to GitHub with
    significant improvements.
    
    Note: Development history was squashed per organizational policy when
    moving between version control systems. Original private repository
    contained 100+ commits.

--- frontend/README.md
+++ frontend/README.md
@@ -1,16 +1,33 @@
-# frontend
-
-A new Flutter project.
-
+# Meditation App
 ## Getting Started
 
-This project is a starting point for a Flutter application.
+### Setup Guide for Linux
 
-A few resources to get you started if this is your first Flutter project:
+1. Install Flutter Version Management (FVM):
+```
+curl -fsSL https://fvm.app/install.sh | bash
+```
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+2. Clone the Meditation App repository:
+```
+git clone https://github.com/Lntck/Meditation-App.git
+cd Meditation-App
+```
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+3. Use FVM to install and set up Flutter:
+```
+fvm install
+fvm use stable
+```
+
+4. Get dependencies:
+```
+fvm flutter pub get
+```
+
+5. Run the app:
+```
+fvm flutter run -d linux
+```
+
+
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,122 +1,143 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:meditation_app/providers/user_provider.dart';
+import 'package:meditation_app/widgets/centered_max_width_widget.dart';
+import 'package:localization/localization.dart';
+import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
+import 'router/router.dart';
+import 'utils/platform_checker.dart';
 
-void main() {
-  runApp(const MyApp());
+const String title = 'Inner Breeze';
+final GlobalKey<AppState> appKey = GlobalKey();
+
+void run() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+      child: App(key: appKey),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+  if (isDesktop()) {
+    await windowManager.ensureInitialized();
+
+    WindowOptions windowOptions = WindowOptions(
+      size: Size(420, 620),
+      center: true,
+      title: title,
+      backgroundColor: Colors.transparent,
+      skipTaskbar: false,
+      titleBarStyle: TitleBarStyle.normal,
     );
+
+    windowManager.waitUntilReadyToShow(windowOptions, () async {
+      await windowManager.show();
+      await windowManager.focus();
+
+      run();
+    });
+  } else {
+    run();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+final _darkTheme = ThemeData.from(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.teal,
+    brightness: Brightness.dark,
+  ),
+);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+final _lightTheme = ThemeData.from(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: Colors.teal,
+    brightness: Brightness.light,
+  ),
+);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class App extends StatefulWidget {
+  const App({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<App> createState() => AppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class AppState extends State<App> {
+  Locale _currentLocale;
+  AppState() : _currentLocale = Locale('en');
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    initializeLocale();
+    if (!isDesktop()) {
+      _preloadAssets(context);
+    }
+  }
+
+  Future<void> initializeLocale() async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    String languageCode = await userProvider.getLanguagePreference();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentLocale = Locale(languageCode);
     });
   }
 
+  Future<void> _preloadAssets(BuildContext context) async {
+    final imageExtensions = ['.png', '.jpg', '.jpeg'];
+
+    final imageAssetFilenames = [
+      'angel.jpg',
+      'begin.jpg',
+      'logo.png',
+    ];
+
+    // Construct full paths for image assets
+    final imageAssetPaths = imageAssetFilenames
+        .map((filename) => 'assets/images/$filename')
+        .toList();
+
+    for (final assetPath in imageAssetPaths) {
+      if (imageExtensions.any((ext) => assetPath.endsWith(ext))) {
+        await precacheImage(AssetImage(assetPath), context);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    LocalJsonLocalization.delegate.directories = ['lib/i18n'];
+
+    return MaterialApp.router(
+      routerConfig: router,
+      debugShowCheckedModeBanner: false,
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        LocalJsonLocalization.delegate,
+      ],
+      supportedLocales: [
+        Locale('en', 'US'),
+        Locale('ru', 'RU'),
+      ],
+      locale: _currentLocale,
+      title: title,
+      theme: _lightTheme,
+      darkTheme: _darkTheme,
+      themeMode: ThemeMode.dark,
+      builder: (context, child) {
+        var backgroundColor = Theme.of(context).colorScheme.surface;
+
+        return Container(
+          color: backgroundColor,
+          child: CenteredMaxWidthWidget(child: child!),
+        );
+      },
     );
   }
 }
